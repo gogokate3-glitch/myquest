6019017e3df3f2a81ca4f28f71f730a4bf96bba3
gogokate3@gmail.com <gogokate3@gmail.com>
2025-12-15 15:17:31 +0900

    デバッグ削除

--- debug_script.py
+++
@@ -1,5 +0,0 @@
-with open('questions.json', 'r', encoding='utf-8') as f:
-    content = f.read()
-with open('debug.txt', 'w', encoding='utf-8') as df:
-    df.write(content[5600:5800])
-print("Wrote a snippet of questions.json to debug.txt")
